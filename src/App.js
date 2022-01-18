1a2b64d6410124f8a6b6348af05ebf9b944e2f60
Joshua <jadrdevcm@gmail.com>
2022-01-18 16:26:39 +0000

    Archivos eliminados de npx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [calc, setCalc] = useState("");
+  const [resultado, setResultado] = useState("");
+
+
+  const operaciones = ['/', '*', '+', '-', '.'];
+
+  const calcular = value => {
+
+    if (
+      operaciones.includes(value) && calc === '' ||
+      operaciones.includes(value) && operaciones.includes(calc.slice(-1))
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+
+    if (!operaciones.includes(value)) {
+      resultado(eval(calc + value).toString())
+    }
+  }
+
+
+  const crearNumeros = () => {
+    const numeros = [];
+
+    for (let i = 1; i < 10; i++) {
+      numeros.push(
+        <button
+          onClick={() => calcular(i.toString())}
+          key={i}>
+          {i}</button>
+      )
+    }
+    return numeros
+  }
+
+  const calcularTotal = () => {
+    setCalc(eval(calc).toString())
+  }
+
+  const BorraUltimo = () => {
+    if (calc === '') {
+      return;
+    }
+
+    const value = calc.slice(0, -1);
+    setCalc(value)
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculadora">
+        <div className="display">
+          {resultado ? <span>({resultado})</span> : ''}
+          {calc || '0'}
+        </div>
+
+        <div className="operadores">
+          <button onClick={() => calcular('/')}>/</button>
+          <button onClick={() => calcular('*')}>*</button>
+          <button onClick={() => calcular('+')}>+</button>
+          <button onClick={() => calcular('-')}>-</button>
+
+          <button onClick={BorraUltimo}>DEL</button>
+        </div>
+
+        <div className="numeros">
+          {crearNumeros()}
+          <button onClick={() => calcular('0')}>0</button>
+          <button onClick={() => calcular('.')}>.</button>
+          <button onClick={calcularTotal}>=</button>
+
+        </div>
+      </div>
     </div>
+
   );
 }
 
